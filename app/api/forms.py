172cf34bd8e72361a364fa8eff2eeaf3200ecec2
g.rahimzhanov <cubicscience@yandex.ru>
2024-06-28 14:29:37 +0300

    added first almost correct version layout signup

--- app/api/forms.py
+++ app/api/forms.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        # Удаление help_text для каждого поля
+        for fieldname in ['username', 'password1', 'password2', 'email', 'organization']:
+            if fieldname in self.fields:
+                self.fields[fieldname].help_text = None
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()  # Указание вашей кастомной модели пользователя
-        fields = UserCreationForm.Meta.fields + ('email', 'organization',)  # Добавление дополнительных полей, если нужно
+        fields = UserCreationForm.Meta.fields + ('email', 'organization',)  # Добавление дополнительных полей
